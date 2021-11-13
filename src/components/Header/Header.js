ec6e96d5c148e8eeb12c615c15ac11aa7f32c289
Knirta <myknirtak@gmail.com>
2021-11-13 12:10:58 +0200

    Header and Container
    Add Header and Container components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,30 @@
-const Header = () => {
-  return <div>Header</div>;
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import Logo from "../Logo";
+import logout from "../../images/svg/logout.svg";
+import "./Header.scss";
+
+const Header = ({ name }) => {
+  return (
+    <header className="Header">
+      <div className="Header__logo">
+        <Logo />
+        <span className="Header__logo-text">Wallet</span>
+      </div>
+      <div className="Header__logout">
+        <span className="Header__username">{name}</span>
+        <Link to="/logout" className="Header__link">
+          <img src={logout} width="18" height="18" alt="logout" />
+          <span className="Header__logout-text">Выйти</span>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+Header.propTypes = {
+  name: PropTypes.string.isRequired,
 };
 
 export default Header;
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,9 @@
+import React from "react";
+import wallet from "../../images/svg/wallet.svg";
+import "./Logo.scss";
+
+const Logo = () => {
+  return <img src={wallet} className="Logo" alt="logo" />;
+};
+
+export default Logo;
