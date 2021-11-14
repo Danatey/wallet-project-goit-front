72f327d3348d3ecd040b9cf013c90ac5929d13ff
Bogdan Dyachenko <46127718+Bodyan-d@users.noreply.github.com>
2021-11-14 17:09:12 +0200

    connect-fonts

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import RegistrationPage from './pages/RegistrationPage';
 import HomeTab from './components/HomeTab';
 import DiagramTab from './components/DiagramTab';
 import 'node_modules/modern-normalize/modern-normalize.css';
+import './stylesheet/fonts.css';
 import './common/main.scss';
 
 function App() {
