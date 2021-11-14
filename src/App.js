450efe07d0cf1a39009fc9f02d104377a26e214b
Bogdan Dyachenko <46127718+Bodyan-d@users.noreply.github.com>
2021-11-14 17:01:19 +0200

    connect-common

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import { Routes, Route } from "react-router-dom";
-import Container from "./components/Container";
-import DashboardPage from "./pages/DashboardPage";
-import LoginPage from "./pages/LoginPage";
-import RegistrationPage from "./pages/RegistrationPage";
-import HomeTab from "./components/HomeTab";
-import DiagramTab from "./components/DiagramTab";
+import { Routes, Route } from 'react-router-dom';
+import Container from './components/Container';
+import DashboardPage from './pages/DashboardPage';
+import LoginPage from './pages/LoginPage';
+import RegistrationPage from './pages/RegistrationPage';
+import HomeTab from './components/HomeTab';
+import DiagramTab from './components/DiagramTab';
+import 'node_modules/modern-normalize/modern-normalize.css';
+import './common/main.scss';
 
 function App() {
-  return (
-    <Container>
-      <Routes>
-        <Route path="/" element={<DashboardPage />}>
-          <Route path="home" element={<HomeTab />} />
-          <Route path="diagram" element={<DiagramTab />} />
-        </Route>
-        <Route path="login" element={<LoginPage />} />
-        <Route path="register" element={<RegistrationPage />} />
-      </Routes>
-    </Container>
-  );
+	return (
+		<Container>
+			<Routes>
+				<Route path='/' element={<DashboardPage />}>
+					<Route path='home' element={<HomeTab />} />
+					<Route path='diagram' element={<DiagramTab />} />
+				</Route>
+				<Route path='login' element={<LoginPage />} />
+				<Route path='register' element={<RegistrationPage />} />
+			</Routes>
+		</Container>
+	);
 }
 
 export default App;
