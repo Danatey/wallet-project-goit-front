b3bcefc1a0d89198fcfcda36e6bc881601a71980
Данатей <danatey40793@gmail.com>
2021-11-16 22:47:06 +0200

    Вывел запрос курса валют, подключил currency, исправил классы

--- src/components/Currency/Currency.js
+++ src/components/Currency/Currency.js
@@ -8,26 +8,14 @@ import {
   TableCell,
 } from "@material-ui/core";
 import "./Currency.scss";
-// import fetchData from "../"
-
-const fetchData = async () => {
-  try {
-    const response = await fetch(
-      "https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11"
-    );
-    const rates = response.json();
-    return rates;
-  } catch (error) {
-    throw error;
-  }
-};
+import fetchCurrency from "../../assets/API/fetchCurrency/fetchCurrency";
 
 function Currency() {
   const [currency, setCurrency] = useState([]);
 
   const fetch = async () => {
     try {
-      const data = await fetchData();
+      const data = await fetchCurrency();
       const sliced = data.slice(0, -1);
       setCurrency([...sliced]);
     } catch (error) {
@@ -41,35 +29,35 @@ function Currency() {
 
   return (
     <>
-      <div class="currency-sidebar">
-        <TableContainer class="table">
+      <div className="currency-sidebar">
+        <TableContainer className="table">
           <Table size="small">
-            <TableHead class="head">
+            <TableHead className="head">
               <TableRow>
-                <TableCell class="header">Валюта</TableCell>
-                <TableCell align="center" class="header">
+                <TableCell className="header">Валюта</TableCell>
+                <TableCell align="center" className="header">
                   Покупка
                 </TableCell>
-                <TableCell align="center" class="header">
+                <TableCell align="center" className="header">
                   Продажа
                 </TableCell>
               </TableRow>
             </TableHead>
-            <TableBody class="body">
+            <TableBody className="body">
               {currency?.map((element) => (
                 <TableRow key={element.ccy}>
                   <TableCell
                     component="th"
                     scope="row"
                     align="left"
-                    class="currency"
+                    className="currency"
                   >
                     {element.ccy}
                   </TableCell>
-                  <TableCell align="center" class="buy">
+                  <TableCell align="center" className="buy">
                     {Math.floor(element.buy * 100) / 100}
                   </TableCell>
-                  <TableCell align="center" class="sell">
+                  <TableCell align="center" className="sell">
                     {Math.floor(element.sale * 100) / 100}
                   </TableCell>
                 </TableRow>
--- src/pages/DashboardPage/DashboardPage.js
+++ src/pages/DashboardPage/DashboardPage.js
@@ -2,6 +2,7 @@ import { Outlet } from "react-router";
 import Header from "../../components/Header";
 import Navigation from "../../components/Navigation";
 import Balance from "../../components/Balance";
+import Currency from "../../components/Currency/Currency";
 
 const DashboardPage = () => {
   return (
@@ -10,10 +11,8 @@ const DashboardPage = () => {
       <main>
         <aside>
           <Navigation />
-          {
-            <Balance />
-            /* <Currency /> */
-          }
+          <Balance />
+          <Currency />
         </aside>
         <article>
           <Outlet />
