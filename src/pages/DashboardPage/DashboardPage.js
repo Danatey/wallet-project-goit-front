0bea9dba1719d5d7fd4f89aa0f9d8fe4b205a2e4
Knirta <myknirtak@gmail.com>
2021-11-19 00:20:31 +0200

    hide <Balance>
    hide <Balance> component on mobile on routes /diagram and /currency, add styles

--- src/pages/DashboardPage/DashboardPage.js
+++ src/pages/DashboardPage/DashboardPage.js
@@ -1,21 +1,49 @@
-import { Outlet } from "react-router-dom";
+import React, { useState, useEffect, Fragment } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../../components/Header";
 import Navigation from "../../components/Navigation";
 import Balance from "../../components/Balance";
 import Currency from "../../components/Currency/Currency";
+import Media from "react-media";
 import "./Dashboard.scss";
 
 const DashboardPage = () => {
+  const location = useLocation();
+  const path = location.pathname;
+  const [display, setDisplay] = useState();
+
+  useEffect(() => {
+    setDisplay(path === "/home" ? true : false);
+  }, [path]);
+
   return (
     <>
       <Header />
       <main className="Dashboard__wrap">
-        <aside>
-          <Navigation />
-          <Balance />
-          <Currency />
+        <aside className="Dashboard__aside">
+          <section className="Dashboard__nav">
+            <Navigation />
+            <Media
+              queries={{
+                mobile: { maxWidth: 767 },
+                other: { minWidth: 768 },
+              }}
+            >
+              {(matches) => {
+                return (
+                  <Fragment>
+                    {matches.mobile && display && <Balance />}
+                    {matches.other && <Balance />}
+                  </Fragment>
+                );
+              }}
+            </Media>
+          </section>
+          <section className="Dashboard__currency">
+            <Currency />
+          </section>
         </aside>
-        <article>
+        <article className="Dashboard__main">
           <Outlet />
         </article>
       </main>
