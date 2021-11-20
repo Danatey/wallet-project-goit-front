847c175dac9d4f2fe4ccae408888917b6a0bf4c0
leshakrikun <lesha.krikun@gmail.com>
2021-11-20 02:36:29 +0200

    добавил в таблицу уникальные идентификаторы

--- src/components/DiagramTab/TableStats.js
+++ src/components/DiagramTab/TableStats.js
@@ -50,50 +50,62 @@ const Month = () => {
       data,
     })
     
-
     // Render the UI for your table
     return (
       <table {...getTableProps()} className={s.tableStyle}>
         <thead className={s.tableHeaderStyle}>
-          {headerGroups.map(headerGroup => (
-            <tr className={s.tableHeader} {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps()} className={s.rowStatsStyle}>{column.render('Header')}</th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()} className={s.bodyStatsStyle}>
-          {rows.map((row, i) => {
-            prepareRow(row)
+          {headerGroups.map((headerGroup) => {
+            const { key, ...restHeaderGroupProps } =
+              headerGroup.getHeaderGroupProps();
             return (
-              <tr {...row.getRowProps()} className={s.trStatsStyle}>
-                {row.cells.map(cell => {
-                  return    <td {...cell.getCellProps()} className={s.columnStatsStyle}>
-                                <span  className={s.rectangle}  style={{width: 24 + 'px', height: 24 + 'px', backgroundColor: colors[i]}}></span>
-                                {cell.render('Cell')}
-                            </td>
+              <tr key={key} {...restHeaderGroupProps} className={s.tableHeader}>
+                {headerGroup.headers.map((column) => {
+                  const { key, ...restColumn } = column.getHeaderProps();
+                  return (
+                    <th key={key} {...restColumn} className={s.rowStatsStyle}>
+                      {column.render("Header")}
+                    </th>
+                  );
                 })}
               </tr>
-            )
+            );
+          })}
+        </thead>
+        <tbody {...getTableBodyProps} className={s.bodyStatsStyle}>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            const { key, ...restRowProps } = row.getRowProps();
+            return (
+              <tr key={key} {...restRowProps} className={s.trStatsStyle}> 
+                {row.cells.map((cell) => {
+                  const { key, ...restCellProps } = cell.getCellProps();
+                  return (
+                    <td key={key} {...restCellProps} className={s.columnStatsStyle}>
+                      <span  className={s.rectangle}  style={{width: 24 + 'px', height: 24 + 'px', backgroundColor: colors[i]}}></span>
+                      {cell.render("Cell")}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
           })}
         </tbody>
         <tfoot className={s.tableFooter}>
-        {sumIncome.map(cash => (
-          <tr className={s.tableFooterCash}>
-            <td className={s.columnFooterStatsStyle}>
-            {cash.type}
-            </td>
-           <td className={s.cash}>
-           {cash.money}
-           </td>
-         </tr>
-        ))}
-        </tfoot>
-      </table>
-    )
-  }
-  
+            {sumIncome.map((cash, i) => (
+              <tr className={s.tableFooterCash} key={10001 & i}>
+                <td className={s.columnFooterStatsStyle}>
+                {cash.type}
+                </td>
+               <td className={s.cash}>
+               {cash.money}
+               </td>
+             </tr>
+            ))}
+            </tfoot>
+          </table>
+        )
+      }
+
   function TableStats({data, colors, sumIncome}) {
     const columns = React.useMemo(
         () => [
@@ -122,3 +134,4 @@ const Month = () => {
   }
   
 export default TableStats
+
