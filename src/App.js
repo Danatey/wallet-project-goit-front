13825e97c588dd80efb0313d6df40c48197fe9a8
Bogdan Dyachenko <46127718+Bodyan-d@users.noreply.github.com>
2021-11-21 18:14:37 +0200

    add-some-in-loader

--- src/App.js
+++ src/App.js
@@ -1,65 +1,64 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import Media from "react-media";
-import Container from "./components/Container";
-import DashboardPage from "./pages/DashboardPage";
-import LoginPage from "./pages/LoginPage";
-import RegistrationPage from "./pages/RegistrationPage";
-import HomeTab from "./components/HomeTab";
-import DiagramTab from "./components/DiagramTab";
-import PrivateOutlet from "./components/PrivateRoute";
-import PublicOutlet from "./components/PublicRoute";
-import Currency from "./components/Currency";
-import Loader from "./components/Loader";
-import { authOperations } from "./redux/auth";
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import Media from 'react-media';
+import Container from './components/Container';
+import DashboardPage from './pages/DashboardPage';
+import LoginPage from './pages/LoginPage';
+import RegistrationPage from './pages/RegistrationPage';
+import HomeTab from './components/HomeTab';
+import DiagramTab from './components/DiagramTab';
+import PrivateOutlet from './components/PrivateRoute';
+import PublicOutlet from './components/PublicRoute';
+import Currency from './components/Currency';
+import Loader from './components/Loader';
+import { authOperations } from './redux/auth';
 
-import "./App.scss";
+import './App.scss';
 
 function App() {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(authOperations.getCurrentUser());
-  }, [dispatch]);
+	useEffect(() => {
+		dispatch(authOperations.getCurrentUser());
+	}, [dispatch]);
 
-  return (
-    <Container>
-      <Routes>
-        {/* це приватний раут, поки що залишаю відкритим, щоб зручно було кодить, коли буде працювати логін, тоді розкоментую */}
-        <Route path="/" element={<PrivateOutlet />}>
-          <Route element={<DashboardPage />}>
-            <Route index element={<Navigate to="/home" />} />
-            <Route path="home" element={<HomeTab />} />
-            <Route path="diagram" element={<DiagramTab />} />
-          </Route>
-        </Route>
-        {/* тимчасово звичайний раут, не приватний */}
-        <Route path="/" element={<DashboardPage />}>
-          <Route index element={<Navigate to="/home" />} />
-          <Route path="home" element={<HomeTab />} />
-          <Route path="diagram" element={<DiagramTab />} />
-          <Route
-            path="currency"
-            element={
-              <Media query={{ maxWidth: 767 }}>
-                {(matches) =>
-                  matches ? <Currency /> : <Navigate to="/home" />
-                }
-              </Media>
-            }
-          />
-        </Route>
-        <Route path="login" element={<PublicOutlet restricted />}>
-          <Route index element={<LoginPage />} />
-        </Route>
-        <Route path="register" element={<PublicOutlet restricted />}>
-          <Route index element={<RegistrationPage />} />
-        </Route>
-        <Route path="*" element={<LoginPage />} />
-      </Routes>
-    </Container>
-  );
+	return (
+		<Container>
+			<Loader />
+			<Routes>
+				{/* це приватний раут, поки що залишаю відкритим, щоб зручно було кодить, коли буде працювати логін, тоді розкоментую */}
+				<Route path='/' element={<PrivateOutlet />}>
+					<Route element={<DashboardPage />}>
+						<Route index element={<Navigate to='/home' />} />
+						<Route path='home' element={<HomeTab />} />
+						<Route path='diagram' element={<DiagramTab />} />
+					</Route>
+				</Route>
+				{/* тимчасово звичайний раут, не приватний */}
+				<Route path='/' element={<DashboardPage />}>
+					<Route index element={<Navigate to='/home' />} />
+					<Route path='home' element={<HomeTab />} />
+					<Route path='diagram' element={<DiagramTab />} />
+					<Route
+						path='currency'
+						element={
+							<Media query={{ maxWidth: 767 }}>
+								{matches => (matches ? <Currency /> : <Navigate to='/home' />)}
+							</Media>
+						}
+					/>
+				</Route>
+				<Route path='login' element={<PublicOutlet restricted />}>
+					<Route index element={<LoginPage />} />
+				</Route>
+				<Route path='register' element={<PublicOutlet restricted />}>
+					<Route index element={<RegistrationPage />} />
+				</Route>
+				<Route path='*' element={<LoginPage />} />
+			</Routes>
+		</Container>
+	);
 }
 
 export default App;
--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,16 +1,16 @@
-import React from "react";
-import Loader from "react-loader-spinner";
+import React from 'react';
+import Loader from 'react-loader-spinner';
 
 const LoaderComponent = () => {
-  return (
-    <Loader
-      type="ThreeDots"
-      color="#4A56E2"
-      height={100}
-      width={100}
-      timeout={300} //3 secs
-    />
-  );
+	return (
+		<Loader
+			type='Circles'
+			color='#4A56E2'
+			height={100}
+			width={100}
+			timeout={3000} //3 secs
+		/>
+	);
 };
 
 export default LoaderComponent;
