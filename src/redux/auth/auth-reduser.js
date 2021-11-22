0dbd2190d5aff8a44dabce534305deee41442f65
Knirta <myknirtak@gmail.com>
2021-11-22 16:44:07 +0200

    no user in response on login, only token

--- src/redux/auth/auth-reduser.js
+++ src/redux/auth/auth-reduser.js
@@ -6,7 +6,7 @@ import { changeBalance } from "./auth-actions";
 const initialUserState = { id: null, name: null, email: null, balance: null };
 
 const user = createReducer(initialUserState, {
-  [logIn.fulfilled]: (_, { payload }) => payload.user,
+  // [logIn.fulfilled]: (_, { payload }) => payload.user,
   [register.fulfilled]: (_, { payload }) => payload.user,
   [logOut.fulfilled]: () => initialUserState,
   [getCurrentUser.fulfilled]: (_, { payload }) => payload,
