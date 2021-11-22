33037893e083a19ef3a03fc42a31c9a14e5da8b9
Данатей <danatey40793@gmail.com>
2021-11-22 19:47:04 +0200

    Починил мобильную таблицу

--- src/components/HomeTab/HomeTabMobile.js
+++ src/components/HomeTab/HomeTabMobile.js
@@ -36,38 +36,48 @@ const HomeTabMobile = () => {
   }, [dispatch]);
 
   return (
-    <table className="HomeTab-mobile" {...getTableProps()}>
-      {rows.map((row, i) => {
-        return (
-          <tbody key={row.id} {...getTableBodyProps()}>
-            <tr>
-              <td className="HomeTab-column-header">Дата</td>
-              <td className="HomeTab-column">{data[i].date}</td>
-            </tr>
-            <tr>
-              <td className="HomeTab-column-header">Тип</td>
-              <td className="HomeTab-column">{data[i].type}</td>
-            </tr>
-            <tr>
-              <td className="HomeTab-column-header">Коментарий</td>
-              <td className="HomeTab-column">{data[i].comment}</td>
-            </tr>
-            <tr>
-              <td className="HomeTab-column-header">Категория</td>
-              <td className="HomeTab-column">{data[i].category}</td>
-            </tr>
-            <tr>
-              <td className="HomeTab-column-header">Сумма</td>
-              <td className="HomeTab-column">{data[i].amount}</td>
-            </tr>
-            <tr>
-              <td className="HomeTab-column-header">Баланс</td>
-              <td className="HomeTab-column">{data[i].balance}</td>
-            </tr>
-          </tbody>
-        );
-      })}
-    </table>
+    <>
+      <div className="HomeTab-mobile">
+        {rows.map((row, i) => {
+          return (
+            <table className="HomeTab-mobile_table color" {...getTableProps()}>
+              <tbody
+                className="HomeTab-mobile_table color"
+                key={row.id}
+                {...getTableBodyProps()}
+              >
+                <tr className="HomeTab-row">
+                  <td className="HomeTab-column-header">Дата</td>
+                  <td className="HomeTab-column">{data[i].date}</td>
+                </tr>
+                <tr className="HomeTab-row">
+                  <td className="HomeTab-column-header">Тип</td>
+                  <td className="HomeTab-column">{data[i].type}</td>
+                </tr>
+                <tr className="HomeTab-row">
+                  <td className="HomeTab-column-header">Коментарий</td>
+                  <td className="HomeTab-column">{data[i].comment}</td>
+                </tr>
+                <tr className="HomeTab-row">
+                  <td className="HomeTab-column-header">Категория</td>
+                  <td className="HomeTab-column ">{data[i].category}</td>
+                </tr>
+                <tr className="HomeTab-row">
+                  <td className="HomeTab-column-header">Сумма</td>
+                  <td className="HomeTab-column pink green">
+                    {data[i].amount}
+                  </td>
+                </tr>
+                <tr className="HomeTab-row">
+                  <td className="HomeTab-column-header">Баланс</td>
+                  <td className="HomeTab-column">{data[i].balance}</td>
+                </tr>
+              </tbody>
+            </table>
+          );
+        })}
+      </div>
+    </>
   );
 };
 export { HomeTabMobile };
